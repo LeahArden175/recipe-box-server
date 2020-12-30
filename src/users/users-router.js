d7a9c60a76ca433a30765c3b664728fc53a7f019
Leah Arden <leaharden175@gmail.com>
2020-12-30 15:53:03 -0500

    basic post/users works

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const UsersService = require("./users-service");
+const { json } = require("express");
 
 const usersRouter = express.Router();
 const jsonParser = express.json();
@@ -14,6 +15,27 @@ usersRouter
             res.json(users);
           })
           .catch(next);
+      })
+      .post(jsonParser, (req, res, next) => {
+          const{full_name, username, password} = req.body;
+          const newUser = {full_name, username, password};
+
+          for(const [key, value] of Object.entries(newUser))
+            if(value == null)
+                return res.status(400).json({
+                    error: `Missing ${key} in request body`
+                });
+
+            return UsersService.insertUser(req.app.get('db'), newUser)
+                .then(user => {
+                    res
+                        .status(201)
+                        .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                        .json((user) => {
+                            res.json(user)
+                        })
+                })
+                .catch(next)
       })
 
     module.exports = usersRouter
--- src/users/users-service.js
+++ src/users/users-service.js
@@ -3,6 +3,13 @@ const UsersService = {
     getAllUsers(knex) {
         return knex.select('*').from('recipe_box_users')
     },
+    insertUser( knex, newUser) {
+        return knex
+            .insert(newUser)
+            .into('recipe_box_users')
+            .returning('*')
+            .then(([user]) => user)
+    }
 }
 
 module.exports = UsersService
