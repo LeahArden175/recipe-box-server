28fbfe48394ae9bf67f80a91f10392fdb0d601d4
Leah Arden <leaharden175@gmail.com>
2021-01-04 16:27:50 -0500

    delete tag done

--- src/tags/tags-router.js
+++ src/tags/tags-router.js
@@ -24,6 +24,58 @@ tagsRouter
             })
             .catch(next)
     })
+    .post(jsonParser, (req, res, next) => {
+        const {tag_name} = req.body;
+        const newTag = {tag_name}
 
+        if(!newTag) {
+            res.status(400).json({
+                error: {message : 'Missing tag_name in requestbody'}
+            })
+        }
+
+        TagsService.addTag(
+            req.app.get('db'),
+            newTag
+        )
+        .then(tag => {
+            res
+                .status(201)
+                .location(path.posix.join(req.originalUrl, `/${tag.id}`))
+                .json(serializeTag(tag))
+        })
+        .catch(next)
+    })
+tagsRouter
+    .route('/:tag_id')
+    .all((req, res, next)=> {
+        TagsService.getById(
+            req.app.get('db'),
+            req.params.tag_id
+        )
+        .then(tag => {
+            if(!tag) {
+                return res.status(404).json({
+                    error: {message: 'Tag does not exist'}
+                })
+            }
+            res.tag = tag
+            next()
+        })
+        .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(serializeTag(res.tag))
+    })
+    .delete((req, res, next) => {
+        TagsService.deleteTag(
+            req.app.get('db'),
+            req.params.tag_id
+        )
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(next)
+    })
 
 module.exports = tagsRouter;
--- src/tags/tags-service.js
+++ src/tags/tags-service.js
@@ -5,6 +5,27 @@ TagsService = {
         return knex
             .select('*')
             .from('tags')
+    },
+    addTag(knex, newTag) {
+        return knex
+            .insert(newTag)
+            .into('tags')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    getById(knex, id) {
+        return knex
+            .from('tags')
+            .select('*')
+            .where({ id })
+            .first()
+    },
+    deleteTag(knex, id) {
+        return knex('tags')
+            .where({ id })
+            .delete()
     }
 }
 
