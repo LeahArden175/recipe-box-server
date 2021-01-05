a1fbb4aeb096655c828d77b29a0aa34ca3f172fc
Leah Arden <leaharden175@gmail.com>
2021-01-05 13:54:42 -0500

    auth tests done!

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -4,47 +4,84 @@ const app = require("../src/app");
 const helpers = require("./test-helpers");
 const supertest = require("supertest");
 
-describe('Auth Endpoints', function () {
-    let db;
+describe("Auth Endpoints", function () {
+  let db;
 
-    const { testUsers } =  helpers.makeRecipesFixtures();
-    const testUser = testUsers[0];
+  const { testUsers } = helpers.makeRecipesFixtures();
+  const testUser = testUsers[0];
 
-    before('make knex instance', () => {
-        db = knex({
-            client: 'pg',
-            connection: process.env.TEST_DATABASE_URL
-        })
-        app.get('db', db)
+  before("make knex instance", () => {
+    db = knex({
+      client: "pg",
+      connection: process.env.TEST_DATABASE_URL,
     });
+    app.set("db", db);
+  });
 
-    after('disconnect from db', () => db.destroy());
+  after("disconnect from db", () => db.destroy());
 
-    before('cleanup', () => helpers.cleanTables(db));
+  before("cleanup", () => helpers.cleanTables(db));
 
-    afterEach('cleanup', () =>  helpers.cleanTables(db));
+  afterEach("cleanup", () => helpers.cleanTables(db));
 
-    describe.only('POST /api/auth/login', () => {
-        beforeEach('insert users', () => helpers.seedUsers(db, testUsers));
+  describe("POST /api/auth/login", () => {
+    beforeEach("insert users", () => helpers.seedUsers(db, testUsers));
 
-        const requiredFields = ['username', 'password'];
+    const requiredFields = ["username", "password"];
 
-        requiredFields.forEach((field) => {
-            const loginAttemptBody = {
-                username: testUser.username,
-                password: testUser.password
-            };
+    requiredFields.forEach((field) => {
+      const loginAttemptBody = {
+        username: testUser.username,
+        password: testUser.password,
+      };
 
-            it(`responds with 400 and required error when ${field} is missing`, () => {
-                delete loginAttemptBody[field]
+      it(`responds with 400 and required error when ${field} is missing`, () => {
+        delete loginAttemptBody[field];
 
-                return supertest(app)
-                    .post('/api/auth/login')
-                    .send(loginAttemptBody)
-                    .expect(400, {
-                        error: `Missing ${field} in request body`
-                    })
-            })
-        })
-    })
-})
+        return supertest(app)
+          .post("/api/auth/login")
+          .send(loginAttemptBody)
+          .expect(400, {
+            error: `Missing ${field} in request body`,
+          });
+      });
+      it("responds with 400 error when bad username", () => {
+        const invalidUser = { username: "WRONG", password: "existy" };
+        return supertest(app)
+          .post("/api/auth/login")
+          .send(invalidUser)
+          .expect(400, { error: "Incorrect username or password" });
+      });
+      it("responds with 400 and error when bad password", () => {
+        const userInvalidPass = {
+          username: testUser.username,
+          password: "WRONG",
+        };
+        return supertest(app)
+          .post("/api/auth/login")
+          .send(userInvalidPass)
+          .expect(400, { error: "Incorrect username or password" });
+      });
+      it('responds with 200 and JWT token using secret when valid creds', () => {
+        const userValidCreds = {
+          username : testUser.username,
+          password : testUser.password
+        }
+        const expectedToken = jwt.sign(
+          {user_id: testUser.id},
+          process.env.JWT_SECRET,
+          {
+            subject: testUser.username,
+            algorithm: "HS256"
+          }
+        )
+        return supertest(app)
+          .post('/api/auth/login')
+          .send(userValidCreds)
+          .expect(200, {
+            authToken: expectedToken
+          })
+      })
+    });
+  });
+});
